65b0d1cd815e0b53da7c8aef16c63be2cf009964
mahiol <daringmahesh007@gmail.com>
2019-12-05 17:03:39 +0530

    Modified App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import youtube from '../apis/Youtube';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    state = { videos: [],selectedVideo: null };
+    componentDidMount() {
+        this.onSearchSubmit('Web Development');
+    }
+    onSearchSubmit = async term => {
+        const response = await youtube.get('/search',{
+            params:{
+                q: term
+            }
+        });
+
+        videos: response.data.items,
+        selectedVideo: response.data.items[0]
+        });
+    };
+    onSelectedVideo = video =>{
+        this.setState({selectedVideo: video});
+    }
+        
+    render() {
+        return(
+            
+            <div className="ui container" style={{ marginTop : '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">    
+                            <VideoList videos={this.state.videos} onSelectedVideo={this.onSelectedVideo} />
+                        </div>    
+                    </div>
+                </div>
+            </div>   
+            
+        );
+    }
+    
 }
 
-export default App;
+export default App; 
